6c5fb085afa75aec413a0d87965019d55d3be97c
andrespan <andresabstract@gmail.com>
2023-01-18 14:23:53 -0600

    m0oda

--- R/moda.R
+++ R/moda.R
@@ -1,17 +1,31 @@
 #cargamos la tabla
 library("readr")
 evominining_table <- read_tsv('Evominingtable.tsv')
-columnGLU6P<- evominining_table$`Glycolysis_2--Glucose_6_phosphate_isomerase_4`
+Totalgenomes<- evominining_table$Genomes
+Totalgenomes[]
+class(Totalgenomes)
+genomas <- c("Pseudophaeobacter_5mSIPHEX1_37", "Paracoccus_sp000967825_700mSIPHEX1_1")
+Totalgenomes
 
+
+columnGLU6P
 class(columnGLU6P)
 
 getmoda <- function(v) {
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
-moda <- getmoda(evominining_table$`PYR_THR_AA_1--Alanine_dehydrogenase_2`)
+moda <- getmoda(columnGLU6P)
+
+getmode(columnGLU6P)
 print(moda)
+columnGLU6P==moda
 
-library(dplyr)
 
-evominining_table %>% filter_all(any_vars(. %in% c(moda)))
+which(columnGLU6P==moda)
+
+
+
+row.names
+==genomas
+
