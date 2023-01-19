1bc7760491711c780443a64d22d65df75d3af905
andrespan <andresabstract@gmail.com>
2023-01-19 13:56:34 -0600

    moda

--- R/moda1.R
+++ R/moda1.R
@@ -1,12 +1,14 @@
-#cargamos la tabla
 library("readr")
 evominining_table <- read_tsv('Evominingtable.tsv')
 Totalgenomes<- evominining_table$Genomes
 Totalgenomes[]
 class(Totalgenomes)
 genomas <- c("Pseudophaeobacter_5mSIPHEX1_37", "Paracoccus_sp000967825_700mSIPHEX1_1")
+genomas
+Mydata_bins <- grep("*mSIPHEX*",Totalgenomes)
+Totalgenomes[Mydata_bins]
 #localizar indices de los genomas
-indexes<-which(Totalgenomes%in%genomas)
+indexes<-which(Totalgenomes%in%Mydata_bins)
 indexes
 
 numerical_columns <- sapply(evominining_table, is.numeric)
@@ -18,8 +20,7 @@ getmoda <- function(v) {
 #encontrar todas las modas
 allmodes<-sapply(evominining_table[, numerical_columns], getmoda)
 
-class(allmodes)
-
+allmodes
 #encontrar una columna
 EF <- evominining_table$`ALPHAKETOGLUTARATE_AMINOACIDS_3--Glutamine_synthetase_1`
 EF
@@ -29,9 +30,11 @@ EFmode
 above_mode_indexes <- which(evominining_table$`ALPHAKETOGLUTARATE_AMINOACIDS_3--Glutamine_synthetase_1`> EFmode)
 above_mode_indexes
 #Buscamos index de genomas en la lista de indexes mayor a la moda
-indexes%in%above_mode_indexes
+binsabovemode<-Mydata_bins[which(Mydata_bins%in%above_mode_indexes)]
+Totalgenomes[binsabovemode]
+#Encontrar nombres de lasEF
+EF_names <- colnames(evominining_table)
 
+above_mode_indexes <- function(f){
 
-
-
-
+}
