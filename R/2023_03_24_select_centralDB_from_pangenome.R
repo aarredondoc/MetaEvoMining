95d9d32b76a9c4286b70e1b146107b2b2c85ba65
andrespan <andresabstract@gmail.com>
2023-03-24 16:18:02 -0600

    pangenome matrix

--- R/2023_03_24_select_centralDB_from_pangenome.R
+++ R/2023_03_24_select_centralDB_from_pangenome.R
@@ -1,1 +1,13 @@
-Pangenome_matrix <- read_tsv("Pangenome_matrix")
+library(dplyr)
+library(readr)
+Pangenome_matrix <- read_csv("pangenome_matrix_t0.csv")
+
+# la mitad mas 1. sumar true = no cero
+#filtrar
+rownames(Pangenome_matrix)<-Pangenome_matrix$Gene
+Pangenome_matrix[,15:26]
+
+V <- as.logical(Pangenome_matrix[,15:26])
+sum(V)>length(V)/2
+#para todos los renglones
+filter(Pangenome_matrix,sum(V)>length(V)/2 )
