d1194d2d76f0af9ef3cad6bec227bfcf3d9a43d5
andrespan <andresabstract@gmail.com>
2023-05-08 13:22:22 -0600

    moví carpeta faltantes

--- examples/example.R
+++
@@ -1,7 +0,0 @@
-filter_userID<-function(data_gtdbtk, data_list_names=NULL, data_names=NULL){
-  #Variables------------------------------------------------------------------####
-  gtdbK_file <- read_tsv(data_gtdbtk)
-  return(gtdbK_file)
-}
-library("readr")
-filter_userID("data/gtdbtk.bac120.summary.tsv")
--- examples/noblank.R
+++
@@ -1,7 +0,0 @@
-fasta_lines <- readLines("new2.fasta")
-
-# filter out the blank lines
-non_blank_lines <- fasta_lines[!grepl("^\\s*$", fasta_lines)]
-
-# write the non-blank lines back to the file
-writeLines(non_blank_lines, "noblank_DB.fasta")
