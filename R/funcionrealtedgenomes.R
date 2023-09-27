fe4a205c1befc9f2678d2daceecfbe6da906246e
andrespan <andresabstract@gmail.com>
2023-09-27 10:29:49 -0500

    commit

---
+++ R/funcionrealtedgenomes.R
@@ -0,0 +1,72 @@
+library(readr)
+
+gtdbK_file <- read_tsv("inst/extdata/gtdbtk.bac120.summary.tsv")
+gtdbK_file$`other_related_references(genome_id,species_name,radius,ANI,AF)`
+make_taxonomy_id <- function(table,
+                             assembly_name){
+  # search the row of id in gtdbk_file user_genome column-------------------####
+  assembly_name_index<-table[table$user_genome == assembly_name,]
+
+  # find the row on the gtdbk that corresponds to the classification--------####
+  assembly_classification<-strsplit(assembly_name_index$classification, ";")
+
+  # find the specie assigned or genus or family-----------------------------####
+  specie<-lapply(assembly_classification,`[[`, 7)
+  lineage<-specie
+  if(specie == "s__"){
+    lineage<-lapply(assembly_classification,`[[`, 6)
+    if(specie=="g__"){
+      lineage<-lapply(assembly_classification,`[[`, 5)
+    }
+  }
+
+  # cut specie and separate by __-------------------------------------------####
+  s<-strsplit(as.character(lineage), "__")
+  sp<-s[[1]][2]
+  g_sp<- gsub(" ", "_", sp)
+
+  # change the - for _ on id which are in user_genome-----------------------####
+  id_completo<-gsub("-", "_", assembly_name_index$user_genome)
+
+  # correct the ID----------------------------------------------------------####
+  id_split <-strsplit(id_completo, "_")
+  bin_id<-paste(id_split[[1]][1], "_", id_split[[1]][3], sep = "")
+
+  # add bin_id to the name of specie to differentiate-----------------------####
+  sp_rast<-paste(g_sp,bin_id,sep = "")
+
+  # assign a number id based on index--------------------------------------####
+  xvalue<-which(table == assembly_name, arr.ind = TRUE)
+  x_row<-xvalue[1]
+  row_size<- nchar(x_row)
+  if(nchar(x_row) == 1){
+    row_size<- as.numeric(x_row)*100000
+  } else {
+    if(nchar(x_row) == 2){
+      row_size<- as.numeric(x_row)*11000
+    }
+  }
+  value_id<-format(row_size, scientific = FALSE)
+  feature_id<-paste("666666.",value_id,sep = "")
+
+  fam<-lapply(assembly_classification,`[[`, 5)
+
+  other_relatedcol<-assembly_name_index$`other_related_references(genome_id,species_name,radius,ANI,AF)`
+  patron <- "(GCF|GCA)_\\d+\\.\\d+"
+  cadenas_encontradas <- grep(patron, unlist(strsplit(other_relatedcol, ", ")), value = TRUE)
+
+  # Mostrar las cadenas encontradas
+  cat(cadenas_encontradas, sep = "; ")
+  # make a row of taxonomy table--------------------------------------------####
+  taxonomy_table_row <- data.frame(matrix(ncol = 5, nrow = 0))
+  colnames(taxonomy_table_row) <- c("id_numero",
+                                    "feature_id",
+                                    "user_genome",
+                                    "gtdbk",
+                                    "family")
+
+  taxonomy_table_row[1,] <- c(value_id, feature_id, bin_id,	sp_rast,fam)
+  return (taxonomy_table_row)
+}
+
+make_taxonomy_id(gtdbK_file,"5mSIPHEX2-concot_16")
---
+++ R/make_taxonomy_table_related.R
@@ -0,0 +1,20 @@
+make_taxonomy_table_related<-function(gtdbK_report){
+
+  # read the file-----------------------------------------------------------####
+  gtdbK_file <- read_tsv(gtdbK_report)
+
+
+  # make the taxonomy_table ------------------------------------------------####
+  userg_list<-gtdbK_file$user_genome
+
+
+  taxonomy_table <- map_df(userg_list, ~ make_taxonomy_id_Related(gtdbK_file, .))
+
+
+  return(taxonomy_table)
+}
+
+
+library(dplyr)
+library(purrr)
+taxtablerelated<-make_taxonomy_table_related("inst/extdata/gtdbtk.bac120.summary.tsv")
